1a46fad5b41fccf3268526fcfa77037637579ea3
cjh <paul911014@naver.com>
2021-03-09 16:54:58 +0900

    edit

--- api/src/main/java/com/nexcloud/api/ui/service/DashboardService.java
+++ api/src/main/java/com/nexcloud/api/ui/service/DashboardService.java
@@ -380,8 +380,8 @@ public class DashboardService {
 
 			// Node Memory Usage (%)
             param									= "";
-			query									= "(100 * (1 - ((avg_over_time(node_memory_MemFree_bytes[10m]) + avg_over_time(node_memory_Cached_bytes[10m]) + avg_over_time(node_memory_Buffers_bytes[10m])) / avg_over_time(node_memory_MemTotal_bytes[10m]))))";
-			entityData								= prometheusClient.getDynamicQuery(query, param );
+			query									= "sum by (kubernetes_node) (100 * (1 - ((avg_over_time(node_memory_MemFree_bytes[5m]) + avg_over_time(node_memory_Cached_bytes[5m]) + avg_over_time(node_memory_Buffers_bytes[5m])) / avg_over_time(node_memory_MemTotal_bytes[5m]))))";
+			entityData								= prometheusClient.getQuery(query, param );
             jsonObject 								= (JSONObject) parser.parse(entityData.getBody());
             attrObj.put("mem", jsonObject.get("data"));
             responseObject.put("node", attrObj); 
