e5d4458a834f54730ac1eb7e912076cf0f0c14e7
charlie <charlie@nexcloud.co.kr>
2021-12-01 16:49:14 +0900

    refactor: Modify accessOpenstack(String port, String uri, String projectName, String domainId)

--- api/src/main/java/com/nexcloud/api/openstack/service/OpenstackService.java
+++ api/src/main/java/com/nexcloud/api/openstack/service/OpenstackService.java
@@ -1,6 +1,9 @@
 package com.nexcloud.api.openstack.service;
 
 import com.nexcloud.api.client.OpenstackClient;
+import com.nexcloud.api.domain.ResponseData;
+import com.nexcloud.util.Const;
+import com.nexcloud.util.Util;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +31,36 @@ public class OpenstackService {
     @Value("${openstack.endpoint}")
     private String ENDPOINT;
 
-    public ResponseEntity<String> accessOpenstack(String port, String uri, String projectName, String domainId) {
+    public ResponseEntity<ResponseData> accessOpenstack(String port, String uri, String projectName, String domainId) {
 
         String targetUrl = ENDPOINT + ":" + port + uri;
-        return openstackClient.executeHttpRequest(targetUrl, projectName, domainId);
+
+        ResponseEntity<ResponseData> response 		= null;
+        ResponseData resData						= new ResponseData();
+        ResponseEntity<String> entityData			= null;
+
+        try {
+            entityData = openstackClient.executeHttpRequest(targetUrl, projectName, domainId);
+
+            resData.setData(entityData.getBody());
+            resData.setStatus("success");
+
+            resData.setResponse_code(entityData.getStatusCodeValue());
+            resData.setMessage(Const.SUCCESS);
+
+            response = new ResponseEntity<>(resData, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resData.setResponse_code(Const.INTERNAL_SERVER_ERROR);
+            resData.setMessage(Const.FAIL);
+            resData.setMessage(Util.makeStackTrace(e));
+            response = new ResponseEntity<>(resData, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return response;
+
+//        String targetUrl = ENDPOINT + ":" + port + uri;
+//        return openstackClient.executeHttpRequest(targetUrl, projectName, domainId);
     }
 
     public ResponseEntity<String> accessOpenstack(String uri, String projectName, String domainId) {
